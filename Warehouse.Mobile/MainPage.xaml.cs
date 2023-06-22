d2e3103342611308daf9586ffcd7396a4597ae00
Илья Панов <panoviliy@gmail.com>
2023-06-22 13:50:18 +0500

    add .net maui mobile

--- Warehouse.Mobile/MainPage.xaml.cs
+++ Warehouse.Mobile/MainPage.xaml.cs
@@ -1,29 +1,46 @@
 ﻿using System.Net.Http.Json;
+using System.Text.Json;
 
 namespace Warehouse.Mobile
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
         List<Item> items;
         public MainPage()
         {
             InitializeComponent();
-            HttpClient httpClient = new HttpClient();
-            items = httpClient.GetFromJsonAsync<List<Item>>("https://localhost:7189/api/Items").Result;
+            
         }
 
+        private async Task<List<Item>> GetItemsAsync()
+        {
+            HttpClient _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://localhost:7189/api/Items");
+            HttpResponseMessage response = _client.GetAsync("/api/Items").Result;
+                string content = await response.Content.ReadAsStringAsync();
+                items = JsonSerializer.Deserialize<List<Item>>(content);
+            return items;
+        }
         private void OnCounterClicked(object sender, EventArgs e)
         {
-            count++;
+            int countItem = int.Parse(countItems.Text);
+            string nameItem = selectItem.SelectedItem.ToString();
+            Item selectedItem = items.Where(c => c.name==nameItem).Select(c=>c).Single();
+            price.Text=(selectedItem.cost * 1.2 * countItem).ToString();
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        private void loadItem_Clicked(object sender, EventArgs e)
+        {
+            List<string> @strings = new List<string>();
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            items = GetItemsAsync().Result;
+            foreach (var item in items)
+            {
+                @strings.Add(item.name.ToString());
+            }
+            selectItem.ItemsSource = @strings;
+            selectItem.SelectedItem = selectItem.Items[0];
         }
     }
-    record Item (long Id, string Name, long Count, double Cost, string Type);
+    record Item (long id, string name, long count, double cost, string type);
 }
